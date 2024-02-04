7601d298ccb3e67ffc489408139703c834a029c6
Salman Ali <salmanpucit35@gmail.com>
2024-02-04 13:43:26 -0800

    added min range to date time picker

--- src/components/DateTimePicker.js
+++ src/components/DateTimePicker.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
 
-const DateTimePicker = ({ value, onChange }) => {
+const DateTimePicker = ({ value, onChange, patientAge, doctorAge }) => {
+  const currentDate = new Date().toISOString().slice(0, 16);
   return (
     <TextField
       type="datetime-local"
@@ -9,6 +10,10 @@ const DateTimePicker = ({ value, onChange }) => {
       onChange={(e) => onChange(e.target.value)}
       InputLabelProps={{
         shrink: true,
+      }}
+      inputProps={{
+        min: '1900-01-01T00:00',
+        max: currentDate,
       }}
     />
   );
